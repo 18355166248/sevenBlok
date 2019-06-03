c1f638220a28e3b2298a6f3b52df3e1a910a9ac6
dandan <18856856168@163.com>
2019-06-03 21:47:21 +0800

    添加代码

---
+++ docs/.vuepress/components/list.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li v-for="(item, index) in list" :key="index">{{ item.name }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props:{
+    list: Array
+  }
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  title: '柒世',
+  description: '一直在路上',
+  port: 8082,
+  base: '/',
+  dest: 'docs/.vuepress/distBlok',
+  head: [
+    ['link', {
+      rel: 'shortcut con',
+      type: "image/x-icon",
+      href: '/favicon.ico'
+    }]
+  ],
+  markdown: {
+    lineNumbers: true
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@public': './public'
+      }
+    }
+  },
+  postcss: {
+
+  },
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    sidebarDepth: 2,
+    displayAllHeaders: true,
+    nav: [{
+        text: '首页',
+        link: '/'
+      },
+      {
+        text: '前端',
+        items: [{
+          text: 'JavaScript',
+          link: '/JavaScript/'
+        }, {
+          text: 'HTML',
+          link: '/HTML/'
+        }]
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/18355166248'
+      }
+    ],
+    sidebar: {
+      '/JavaScript/': [
+        '/JavaScript/',
+        {
+          title: 'JavaScript技巧',
+          children: [
+            '/JavaScript/js28'
+          ]
+        },
+        {
+          title: '数组',
+          children: [
+            '/JavaScript/array1', // 以docs为根目录来查找文件 
+            // 上面地址查找的是：docs>accumulate>JS>test.md 文件
+          ]
+        }
+      ]
+    }
+  }
+}
