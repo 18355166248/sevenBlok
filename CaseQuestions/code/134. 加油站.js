bc2dc2484bb62b5abe2bcfa4d3b0b474cbe40dc5
jianglang <643546122@qq.com>
2021-08-29 23:01:00 +0800

    feat: 提交 leetcode 134

--- CaseQuestions/code/134. 加油站.js
+++ CaseQuestions/code/134. 加油站.js
@@ -23,3 +23,5 @@ var canCompleteCircuit = function(gas, cost) {
 };
 console.log(canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]));
 // console.log(canCompleteCircuit([2, 3, 4], [3, 4, 3]));
+
+// 总结：如果x到不了y+1（但能到y），那么从x到y的任一点出发都不可能到达y+1。因为从其中任一点出发的话，相当于从0开始加油，而如果从x出发到该点则不一定是从0开始加油，可能还有剩余的油。既然不从0开始都到不了y+1，那么从0开始就更不可能到达y+1了...
