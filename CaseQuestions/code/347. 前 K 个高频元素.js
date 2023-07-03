4c86327fe54f36298dc342262ecdbb67902b7540
jianglang <lang.jiang@ximalaya.com>
2023-07-03 23:54:34 +0800

    feat: 刷题

--- CaseQuestions/code/347. 前 K 个高频元素.js
+++ CaseQuestions/code/347. 前 K 个高频元素.js
@@ -1,3 +1,4 @@
+// 给你一个整数数组 nums 和一个整数 k ，请你返回其中出现频率前 k 高的元素。你可以按 任意顺序 返回答案。
 class MiniHeap {
   constructor() {
     this.heap = [];
@@ -26,7 +27,7 @@ class MiniHeap {
   shiftUp(index) {
     if (index === 0) return; // 到顶部暂停
     const parentIndex = this.getParentIndex(index); // 获取父级索引
-    if (this.heap[parentIndex] > this.heap[index]) {
+    if (this.heap[parentIndex][1] > this.heap[index][1]) {
       // 当前值大于父级索引
       this.swap(index, parentIndex); // 交换位置
       this.shiftUp(parentIndex); // 继续往上
@@ -36,12 +37,12 @@ class MiniHeap {
   shiftDown(i) {
     const leftIndex = this.getLeftIndex(i); // 查询左子树索引
     const rightIndex = this.getRightIndex(i); // 查询右子树索引
-    if (this.heap[leftIndex] < this.heap[i]) {
+    if (this.heap[leftIndex] && this.heap[leftIndex][1] < this.heap[i][1]) {
       // 索引大于左子树互换位置
       this.swap(leftIndex, i);
       this.shiftDown(leftIndex);
     }
-    if (this.heap[rightIndex] < this.heap[i]) {
+    if (this.heap[rightIndex] && this.heap[rightIndex][1] < this.heap[i][1]) {
       // 索引大于右子树互换位置
       this.swap(rightIndex, i);
       this.shiftDown(rightIndex);
@@ -68,13 +69,28 @@ class MiniHeap {
     return this.heap[0];
   }
 }
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+  const map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], map.has(nums[i]) ? map.get(nums[i]) + 1 : 1);
+  }
+  const minHeap = new MiniHeap();
 
-module.exports = MiniHeap;
+  map.forEach((nums, value) => {
+    minHeap.insert([value, nums]);
+    if (minHeap.size() > k) {
+      minHeap.pop();
+    }
+  });
 
-const h = new MiniHeap();
-h.insert(3);
-h.insert(2);
-h.insert(1);
-h.pop();
+  return minHeap.heap.map((v) => v[0]);
+};
 
-// console.log(h.heap);
+console.log(topKFrequent([1, 1, 1, 2, 2, 3, 4], 2)); // [1,2]
+
+// console.log(topKFrequent([1], 1)); // [1]
