5447667bc2ca82591ade60258d3fba36355ae9dd
SMegalo <lang.jiang@ximalaya.com>
2023-07-12 19:43:05 +0800

    feat: 11

---
+++ CaseQuestions/leetcode/44. 通配符匹配.js
@@ -0,0 +1,17 @@
+// 给你一个输入字符串 (s) 和一个字符模式 (p) ，请你实现一个支持 '?' 和 '*' 匹配规则的通配符匹配：
+// '?' 可以匹配任何单个字符。
+// '*' 可以匹配任意字符序列（包括空字符序列）。
+// 判定匹配成功的充要条件是：字符模式必须能够 完全匹配 输入字符串（而不是部分匹配）。
+
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function (s, p) {
+  
+};
+
+console.log(isMatch("aa", "a")); // false
+// console.log(isMatch("aa", "*")); // true
+// console.log(isMatch("cb", "?a")); // false
