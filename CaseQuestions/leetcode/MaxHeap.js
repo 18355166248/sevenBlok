23910dbd2a066dd6e6127884c26f197e325ecd24
jianglang <lang.jiang@ximalaya.com>
2023-11-28 23:09:29 +0800

    feat: leetcode 53

--- CaseQuestions/leetcode/MaxHeap.js
+++ CaseQuestions/leetcode/MaxHeap.js
@@ -1,53 +1,62 @@
 class MaxHeap {
   constructor(nums) {
     this.heap = nums;
-
-    for (let i = this.findParentIndex(nums.length); i >= 0; i--) {
+    // 从最低非根节点往上排序
+    for (let i = this.getParentIndex(nums.length); i >= 0; i--) {
       this.shiftUp(i);
     }
   }
 
-  findParentIndex(i) {
-    return Math.floor(i / 2) - 1;
+  getParentIndex(i) {
+    return Math.floor(i / 2) + 1;
   }
-  findLeftIndex(i) {
+  getLeftIndex(i) {
     return i * 2 + 1;
   }
-  findRightIndex(i) {
+  getRightIndex(i) {
     return i * 2 + 2;
   }
   shiftUp(i) {
-    const left = this.findLeftIndex(i);
-    const right = this.findRightIndex(i);
-    const size = this.size();
+    const left = this.getLeftIndex(i);
+    const right = this.getRightIndex(i);
+    const length = this.size();
     let target = i;
-    if (left < size && this.heap[left] > this.heap[target]) {
+    if (left < length && this.heap[left] > this.heap[target]) {
       target = left;
     }
-    if (right < size && this.heap[right] > this.heap[target]) {
+    if (right < length && this.heap[right] > this.heap[target]) {
       target = right;
     }
     if (target !== i) {
-      this.move(i, target);
+      this.move(target, i);
       this.shiftUp(target);
     }
   }
-  move(i, target) {
+  move(i, j) {
     const temp = this.heap[i];
-    this.heap[i] = this.heap[target];
-    this.heap[target] = temp;
+    this.heap[i] = this.heap[j];
+    this.heap[j] = temp;
   }
-  size() {
-    return this.heap.length;
-  }
-  // 删除堆底 重新从头排列
   shift() {
     this.heap[0] = this.heap.pop();
     this.shiftUp(0);
+  }
+  size() {
+    return this.heap.length;
   }
   peek() {
     return this.heap[0];
   }
 }
 
-module.exports = MaxHeap;
+var findKthLargest = function(nums, k) {
+  const heap = new MaxHeap(nums);
+  console.log(heap);
+  for (i = 0; i < k - 1; i++) {
+    heap.shift();
+  }
+
+  return heap.peek();
+};
+
+console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2));
