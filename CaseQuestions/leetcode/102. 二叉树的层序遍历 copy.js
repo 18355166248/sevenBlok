5b69211063e7ccf50cb55ffbf28e3fa1ef8796f1
jianglang <lang.jiang@ximalaya.com>
2024-04-25 23:59:12 +0800

    feat: 提交ts教程

---
+++ CaseQuestions/leetcode/102. 二叉树的层序遍历 copy.js
@@ -0,0 +1,32 @@
+// 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function(root) {};
+
+const tree = {
+  val: 3,
+  left: {
+    val: 9,
+  },
+  right: {
+    val: 20,
+    left: {
+      val: 15,
+    },
+    right: {
+      val: 7,
+    },
+  },
+};
+
+console.log(levelOrder(tree)); // 2
